f54c0c09007c3b02912f313834199f056b2f74d5
David Castro <davidcastro00@gmail.com>
2018-07-13 11:09:55 -0500

    mod_livepoll project creation.

---
+++ lang/en/mod_livepoll.php
@@ -0,0 +1,44 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     mod_livepoll
+ * @category    string
+ * @copyright   Copyright (c) 2018 Blackboard Inc. (http://www.blackboard.com)
+ * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['livepoll:addinstance'] = 'Add poll';
+$string['livepoll:view'] = 'View poll';
+$string['livepoll:vote'] = 'Vote on poll';
+$string['livepollfieldset'] = 'Live poll settings';
+$string['livepollname'] = 'Live poll name';
+$string['livepollname_help'] = 'Live polls allow live voting of polls.';
+$string['livepollsettings'] = 'Settings';
+$string['missingidandcmid'] = 'Missing id and cmid';
+$string['modulename'] = 'Live poll';
+$string['modulename_help'] = 'This is the content of the help tooltip associated with the newlivepollname field';
+$string['modulenameplural'] = 'Live polls';
+$string['newlivepoll'] = 'New Live poll';
+$string['nonewmodules'] = 'No new Live polls';
+$string['pluginadministration'] = 'Live poll administration';
+$string['pluginname'] = 'Live poll';
+$string['privacy:metadata'] = 'Live poll does not store any personal data';
+$string['view'] = 'View';
